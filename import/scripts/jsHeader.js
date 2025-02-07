43ec3f5149013fde57f98b2e28544f6bf5ccad8c
peu0em <peu0em@gmail.com>
2025-02-08 00:54:08 +0900

    test, theme off

--- import/scripts/jsHeader.js
+++ import/scripts/jsHeader.js
@@ -5,10 +5,12 @@ function getIdFromUrl(){
   let idch = url.replace(/^.+\#/,";");
   return (url==idch)?false:idch.replace(/[^(\w|\-)].*/,";");
 }
-
+/* test
 // theme
 document.body.dataset.theme = 
 window.localStorage.getItem("theme")?window.localStorage.getItem("theme"):
 window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":
 "default"
 ;
+
+*/
