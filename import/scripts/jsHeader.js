004d447e0c8faf0a680e0a827a1b9c888a77c864
peu0em <peu0em@gmail.com>
2025-02-08 01:10:58 +0900

    disalbe localstorage

--- import/scripts/jsHeader.js
+++ import/scripts/jsHeader.js
@@ -8,7 +8,7 @@ function getIdFromUrl(){
 
 // theme
 {
-  let themeItem = window.localStorage.getItem("theme");
+  /* let themeItem = window.localStorage.getItem("theme"); */
   document.body.dataset.theme = /* themeItem?
     themeItem:( */
       window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default" /*
