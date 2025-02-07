80e8b034a05a4edb1d19a7341c0cc2f02f5aa6a8
peu0em <peu0em@gmail.com>
2025-02-08 01:20:39 +0900

    disable but dataset

--- import/scripts/jsPrerender.js
+++ import/scripts/jsPrerender.js
@@ -1,9 +1,9 @@
 // theme
 {
   /* let themeItem = window.localStorage.getItem("theme"); */
-  document.body.dataset.theme = /* themeItem?
-    themeItem:( */
-      window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default" /*
+  document.body.dataset.theme = "default" /* themeItem?
+    themeItem:(
+      window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default"
     ) */
   ;
 }
