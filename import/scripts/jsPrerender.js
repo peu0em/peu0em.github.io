9369ae7e49dcb39e1a0b09ce99cbd4b9de3fd4cf
peu0em <peu0em@gmail.com>
2025-02-08 01:27:37 +0900

    all disable again

--- import/scripts/jsPrerender.js
+++ import/scripts/jsPrerender.js
@@ -1,6 +1,5 @@
 // theme
 {
-  console.log("test");
   //let themeItem = window.localStorage.getItem("theme");
   //console.log(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default");
   /* document.body.dataset.theme = "default" themeItem?
