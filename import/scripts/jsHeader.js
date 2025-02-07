1a9ff896b4b72c9c27fd29a923f6e0b76ae8df7f
peu0em <peu0em@gmail.com>
2025-02-08 01:08:23 +0900

    disable local

--- import/scripts/jsHeader.js
+++ import/scripts/jsHeader.js
@@ -9,9 +9,9 @@ function getIdFromUrl(){
 // theme
 {
   let themeItem = window.localStorage.getItem("theme");
-  document.body.dataset.theme = themeItem?
-    themeItem:(
-      //window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default"
-    )
+  document.body.dataset.theme = /* themeItem?
+    themeItem:( */
+      window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default" /*
+    ) */
   ;
 }
