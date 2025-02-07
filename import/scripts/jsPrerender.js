20e80d496c6a33b4947c4f68b6b8e3afef4b2539
peu0em <peu0em@gmail.com>
2025-02-08 01:25:39 +0900

    only console

--- import/scripts/jsPrerender.js
+++ import/scripts/jsPrerender.js
@@ -1,7 +1,8 @@
 // theme
 {
-  let themeItem = window.localStorage.getItem("theme");
-  console.log(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default");
+  console.log("test");
+  //let themeItem = window.localStorage.getItem("theme");
+  //console.log(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default");
   /* document.body.dataset.theme = "default" themeItem?
     themeItem:(
       window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default"
