4e86c36fed46fb579913b81ddcaba155da9779a4
peu0em <peu0em@gmail.com>
2025-02-07 10:30:39 +0900

    no slash in front of parameter

--- import/scripts/functions/search.js
+++ import/scripts/functions/search.js
@@ -6,7 +6,7 @@ function searchStringChange(str) {
 function upbarSearchF(){
   upbarSearchTyped = document.getElementById("upbar-search-bar").value;
   if(!/^\s*\t*$/.test(upbarSearchTyped)){
-    location.href='/list/?search='+searchStringChange(upbarSearchTyped);
+    location.href='/list?search='+searchStringChange(upbarSearchTyped);
   }
 }
 function upbarSearchIsitenter(cPressed){if (cPressed == 13){upbarSearchF();}}
