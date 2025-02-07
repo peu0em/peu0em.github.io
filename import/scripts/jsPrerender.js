934d860018e411dc36bbee452793032f967e0a0e
peu0em <peu0em@gmail.com>
2025-02-08 01:32:35 +0900

    dont link js

--- import/scripts/jsPrerender.js
+++ import/scripts/jsPrerender.js
@@ -1,10 +1,8 @@
 // theme
 {
-  //let themeItem = window.localStorage.getItem("theme");
-  //console.log(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default");
-  /* document.body.dataset.theme = "default" themeItem?
-    themeItem:(
-      window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default"
-    ) */
+  let themeItem = window.localStorage.getItem("theme");
+  document.body.dataset.theme =
+    themeItem?
+      themeItem:(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"default")
   ;
 }
