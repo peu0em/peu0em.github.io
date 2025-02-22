11cf4a07fc41d9e477bc57485f81abb6aea1386c
peu0em <peu0em@gmail.com>
2025-02-23 04:20:21 +0900

    post

--- import/scripts/functions/sidebar.js
+++ import/scripts/functions/sidebar.js
@@ -1,8 +1,8 @@
 function sidebar_mc(id){
   if(id){
     let main = document.getElementsByClassName("sidebar-main")[0];
-    document.getElementById(main.dataset.display).setAttribute("hidden","");
-    document.getElementById(id).removeAttribute("hidden");
+    document.getElementById(main.dataset.display).hidden = true;
+    document.getElementById(id).hidden = false;
     main.dataset.display = id;
   }
 }
