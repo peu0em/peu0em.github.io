3350f49ad4c673a7c17c233c6f73e6d6a53de9b7
peu0em <peu0em@gmail.com>
2025-12-29 05:57:24 +0900

    base64

--- import/scripts/functions/base64.js
+++ import/scripts/functions/base64.js
@@ -1,43 +1,67 @@
-const MAXLEN = 100000;
-const memory = new WebAssembly.Memory({ initial: 5 });
-const TEXT_PTR = 1024, BASE64_PTR = TEXT_PTR + MAXLEN;
-let base64Wsm;
+{
+  const MAXLEN = 100000;
+  const memory = new WebAssembly.Memory({ initial: 5 });
+  const TEXT_PTR = 1024, BASE64_PTR = TEXT_PTR + MAXLEN;
+  let base64Wsm;
 
-function listenerOn(){
-  const encodeBox = document.getElementById('encode-box'), decodeBox = document.getElementById('decode-box');
-  if(base64Wsm){
-    const heapBase = base64Wsm.exports.getHeapBase()?base64Wsm.exports.getHeapBase():0;
-    encodeBox.addEventListener('input',()=>{
-      let textOrg = encodeBox.value;
-      if(textOrg.length > 0){
-        const text = new TextEncoder().encode((textOrg.length > MAXLEN)?textOrg.slice(0,MAXLEN):textOrg);
-        const len = text.length;
-        new Uint8Array(memory.buffer,heapBase+TEXT_PTR,len).set(text);
+  function listenerOn(){
+    const encodeBox = document.getElementById('text-box'), decodeBox = document.getElementById('base64-box');
+    if(base64Wsm){
+      const heapBase = base64Wsm.exports.getHeapBase()?base64Wsm.exports.getHeapBase():0;
+      encodeBox.addEventListener('input',()=>{
+        let textOrg = encodeBox.value;
+        if(textOrg.length > 0){
+          const text = new TextEncoder().encode((textOrg.length > MAXLEN)?textOrg.slice(0,MAXLEN):textOrg);
+          const len = text.length;
+          new Uint8Array(memory.buffer,heapBase+TEXT_PTR,len).set(text);
 
-        decodeBox.value = new TextDecoder().decode(new Uint8Array(memory.buffer,heapBase+BASE64_PTR,base64Wsm.exports.base64Encode(BASE64_PTR,TEXT_PTR,len)));
-      }
-    });
-    decodeBox.addEventListener('input',()=>{
-      let textOrg = decodeBox.value;
-      if(textOrg.length > 0){
-        const text = new TextEncoder().encode((textOrg.length > MAXLEN)?textOrg.slice(0,MAXLEN):textOrg);
-        const len = text.length;
-        new Uint8Array(memory.buffer,heapBase+BASE64_PTR,len).set(text);
+          decodeBox.value = new TextDecoder().decode(new Uint8Array(memory.buffer,heapBase+BASE64_PTR,base64Wsm.exports.base64Encode(BASE64_PTR,TEXT_PTR,len)));
+        }
+      });
+      decodeBox.addEventListener('input',()=>{
+        let textOrg = decodeBox.value;
+        if(textOrg.length > 0){
+          const text = new TextEncoder().encode((textOrg.length > MAXLEN)?textOrg.slice(0,MAXLEN):textOrg);
+          const len = text.length;
+          new Uint8Array(memory.buffer,heapBase+BASE64_PTR,len).set(text);
 
-        outLen = base64Wsm.exports.base64Decode(TEXT_PTR,BASE64_PTR,len);
+          outLen = base64Wsm.exports.base64Decode(TEXT_PTR,BASE64_PTR,len);
 
-        encodeBox.value = new TextDecoder().decode(new Uint8Array(memory.buffer,heapBase+TEXT_PTR,outLen));
-      }
-    });
+          encodeBox.value = new TextDecoder().decode(new Uint8Array(memory.buffer,heapBase+TEXT_PTR,outLen));
+        }
+      });
+    }
   }
+
+  (async ()=>{
+    const {instance} = await WebAssembly.instantiateStreaming(await fetch('/import/scripts/functions/base64.wasm'), { env: { memory } });
+    base64Wsm = instance;
+    
+    if(document.readyState == "loading"){
+      document.addEventListener("DOMContentLoaded", listenerOn);
+    } else listenerOn();
+  })();
 }
 
-(async ()=>{
-  const {instance} = await WebAssembly.instantiateStreaming(await fetch('/import/scripts/functions/base64.wasm'), { env: { memory } });
-  base64Wsm = instance;
-  
-  if(document.readyState == "loading"){
-    document.addEventListener("DOMContentLoaded", listenerOn);
-  } else listenerOn();
-})();
+document.addEventListener("DOMContentLoaded",()=>{
+  const textBox = document.getElementById('text-box'), base64Box = document.getElementById('base64-box');
 
+  document.getElementById('text-copy').addEventListener('click',()=>{
+    if(textBox.value){ navigator.clipboard.writeText(textBox.value); }
+  });
+  document.getElementById('text-paste').addEventListener('click',()=>{
+    (async ()=>{
+      const paste = await navigator.clipboard.readText();
+      textBox.value = paste;
+    })();
+  });
+  document.getElementById('base64-copy').addEventListener('click',()=>{
+    if(base64Box.value){ navigator.clipboard.writeText(base64Box.value); }
+  });
+  document.getElementById('text-paste').addEventListener('click',()=>{
+    (async ()=>{
+      const paste = await navigator.clipboard.readText();
+      base64Box.value = paste;
+    })();
+  });
+});
